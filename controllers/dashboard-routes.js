1b9b883214f54e6510f4e8181a6b152760b76aa0
Chidori <business.faissal@outlook.com>
2022-03-29 00:58:34 -0500

    controllers completed

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,49 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', withAuth, (req, res) => {
+  Post.findAll({
+    where: {
+      // use the ID from the session
+      user_id: req.session.user_id,
+    },
+  })
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render('admin-dashboard', { layout: 'dashboard', posts, loggedIn: true });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect('login');
+    });
+});
+
+router.get('/new', withAuth, (req, res) => {
+  res.render('new-post', {
+    layout: 'dashboard',
+  });
+});
+
+router.get('/edit/:id', withAuth, (req, res) => {
+  Post.findByPk(req.params.id)
+    .then((dbPostData) => {
+      if (dbPostData) {
+        const post = dbPostData.get({ plain: true });
+
+        res.render('edit-post', {
+          layout: 'dashboard',
+          post,
+          loggedIn: true,
+        });
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
