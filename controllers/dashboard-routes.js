5c3ce780bb217856b6d1fb633560960286925c57
Chidori <business.faissal@outlook.com>
2022-03-29 01:56:55 -0500

    still need to finish remaining handlebars and add stylesheet

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -12,7 +12,7 @@ router.get('/', withAuth, (req, res) => {
   })
     .then((dbPostData) => {
       const posts = dbPostData.map((post) => post.get({ plain: true }));
-      res.render('admin-dashboard', { layout: 'dashboard', posts, loggedIn: true });
+      res.render('loggedIn-dashboard', { layout: 'dashboard', posts, loggedIn: true });
     })
     .catch((err) => {
       console.log(err);
